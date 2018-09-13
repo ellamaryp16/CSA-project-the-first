024bfedf8f3f39c49aa5896c2d61608802a27d8a
ellamaryp16 <42983257+ellamaryp16@users.noreply.github.com>
2018-09-13 13:22:50 -0500

    hi

--- medialab/Song.java
+++ medialab/Song.java
@@ -8,26 +8,8 @@
 public class Song
 {
     // instance variables - replace the example below with your own
-    private int x;
+    private int rating;
 
-    /**
-     * Constructor for objects of class Song
-     */
-    public Song()
-    {
-        // initialise instance variables
-        x = 0;
+    
     }
 
-    /**
-     * An example of a method - replace this comment with your own
-     *
-     * @param  y  a sample parameter for a method
-     * @return    the sum of x and y
-     */
-    public int sampleMethod(int y)
-    {
-        // put your code here
-        return x + y;
-    }
-}
