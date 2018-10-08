2053ef552318f2fb6c7712b6bc825e91d05d4c04
ellamaryp16 <42983257+ellamaryp16@users.noreply.github.com>
2018-10-08 13:10:25 -0500

    asdlkfg

---
+++ fkja/xclkg.java
@@ -0,0 +1,13 @@
+
+/**
+ * Write a description of class xclkgj here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class xclkg {
+    int fall = 6;
+    int run = 
+    
+    
+}
