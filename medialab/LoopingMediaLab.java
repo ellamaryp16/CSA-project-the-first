d20fe1c6dba0a60160956e1cf5c1701ea116a54f
idel05 <42978812+idel05@users.noreply.github.com>
2018-11-07 13:10:19 -0600

    That ate off someones face

---
+++ medialab/LoopingMediaLab.java
@@ -0,0 +1,18 @@
+
+/**
+ * Write a description of class LoopingMediaLab here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class LoopingMediaLab
+{
+    public static void main ()
+    {
+        System.out.println(MediaFile.readString());
+    for (int i = 0; i<3; i++) {
+        MediaFile.readString();
+    } 
+    }
+  
+}
